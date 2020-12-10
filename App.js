c057808813610bb449244cd2a7d3125707246003
Boubacar7 <boubacarclasss@gmail.com>
2020-12-10 11:10:15 +0100

    1 file modified,

--- App.js
+++ App.js
@@ -51,8 +51,9 @@ export default function App() {
   const handleSubmit = data => {
     console.log(`
       Email: ${data.email}
+      
       Password: ${data.password}
-      Country: ${data.country}
+      
       Accepted Terms: ${data.acceptedTerms}
     `);
 
@@ -64,7 +65,7 @@ export default function App() {
       onSubmit={handleSubmit}
       render={formRenderProps => (
         <form onSubmit={formRenderProps.onSubmit}>
-          <h1>Create AOSC Account</h1>
+          <h1>Create an Account</h1>
 
           <Field
             label="Email Atos"
@@ -90,13 +91,6 @@ export default function App() {
             validator={requiredValidator}
           />
 
-          {/*  <Field 
-            label="Country"
-            name="country"
-            component={CustomDropDown}
-            options={countries}
-            validator={requiredValidator} />
-*/}
           <Field
             label="I accept the terms of service"
             name="acceptedTerms"
